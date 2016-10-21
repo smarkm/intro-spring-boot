e288864f5972f47433c0985ca21c43107b4476a0
Smark <xianchao_meng@huazhikai.com>
2016-10-21 18:09:30 +0800

    a

---
+++ intro-spring-boot-validation/src/main/java/org/smark/opensource/isb/validation/IntroSpringBootValidationApplication.java
@@ -0,0 +1,12 @@
+package org.smark.opensource.isb.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringBootValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringBootValidationApplication.class, args);
+	}
+}
