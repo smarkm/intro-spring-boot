1dc612545dd100e42b4303d58520a50178ea2536
Smark <smark@freecoop.net>
2016-11-17 08:05:10 +0800

    add cache .project

---
+++ intro-spring-boot-cache/src/test/java/org/smark/opensource/isb/cache/jsr107/ConTest.java
@@ -0,0 +1,12 @@
+package org.smark.opensource.isb.cache.jsr107;
+
+import org.junit.Test;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Caching;
+
+public class ConTest {
+
+	@Test
+	public void testName() throws Exception {
+	}
+}
